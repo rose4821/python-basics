f860f499835e6a319df7a7d3bab8db1800e50f7d
rose4821 <161120624+rose4821@users.noreply.github.com>
2024-02-24 15:04:19 +0530

    Add files via upload

---
+++ traffic light simulater.py
@@ -0,0 +1,13 @@
+#project>1:traffic light simulater
+a=(input('Enter any colour:'))
+if(a=='red'):
+    print('stop the vehicle')
+elif(a=='yellow'):
+    print('get ready to go')
+elif(a=='green'):
+    print('you may go now')
+else:
+    print('please enter the correct colour')
+
+
+
